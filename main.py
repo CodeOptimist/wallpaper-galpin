95d6dfd2a77f824c7fc2eca0feba79a1219a9020
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2018-02-24 16:08:57 -0800

    added JSON pagination support added percentage of image used after fitting to a monitor resolution last used images are saved for convenience until next fetch switched from top daily to top weekly already-seen image list saved to data file refactored get_img() and load_wallpaper() with better error handling now uses Pillow's load() to immediately close file handles added support for minute_interval above 60

--- main.py
+++ main.py
@@ -3,67 +3,128 @@ import json
 import os
 import re
 import PIL.ImageOps
+import math
 from PIL import Image
 from collections import OrderedDict
 from galpin_27_36.ahk import helper as ahk
 import time
 import datetime
+import pickle
+import glob
 
-url = "http://www.reddit.com/r/EarthPorn/top.json?t=day&limit=100"
-minute_interval = 60
-data = None
+url = "http://www.reddit.com/r/EarthPorn/top.json?t=week&limit=100"
+after = ""
+minute_interval = 60 * 3
+required_fit_percent = 0.00
+app_dir = r"C:\Working\earthporn"
+app_data = None
+tmp_dir = os.path.join(app_dir, 'tmp')
+json_path = os.path.join(app_dir, 'earthporn.json')
+json_data = None
 
 
 def main():
+    global app_data, after
     monitors, screen = get_monitor_info()
 
-    directory = r"C:\Working\earthporn"
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
+    if not os.path.isdir(app_dir):
+        os.makedirs(app_dir)
+    if not os.path.isdir(tmp_dir):
+        os.mkdir(tmp_dir)
+
+    data_path = os.path.join(app_dir, 'data.bin')
+    if os.path.isfile(data_path):
+        with open(data_path, 'rb') as f:
+            app_data = pickle.load(f)
+    else:
+        app_data = {'accepts': set(), 'rejects': set()}
 
     while True:
-        if update_json(os.path.join(directory, 'earthporn.json')):
-            load_wallpaper(directory, monitors, screen)
+        if update_json():
+            for entry in glob.glob(os.path.join(tmp_dir, '*.*'), recursive=False):
+                if os.path.isfile(entry):
+                    os.remove(entry)
+            if load_wallpaper(monitors, screen):
+                after = ""
+            with open(data_path, 'wb') as f:
+                pickle.dump(app_data, f)
         time.sleep(60)
 
 
-def update_json(path):
-    global data
-    if os.path.isfile(path):
-        age = time.time() - os.path.getmtime(path)
+def update_json():
+    global json_data
+    if os.path.isfile(json_path):
+        age = time.time() - os.path.getmtime(json_path)
         is_recent_file = age <= minute_interval * 60
         if is_recent_file:
-            with open(path, 'r', encoding='utf-8') as f:
-                data = json.load(f)
+            with open(json_path, 'r', encoding='utf-8') as f:
+                json_data = json.load(f)
             return False
 
-    is_time_to_fetch = datetime.datetime.now().time().minute % minute_interval == 0
-    if data is not None and not is_time_to_fetch:
+    is_time_to_fetch = datetime.datetime.now().time().minute % math.gcd(minute_interval, 60) == 0
+    if json_data is not None and not is_time_to_fetch:
         return False
 
-    print(datetime.datetime.now(), "fetching JSON")
-    req = urllib.request.Request(url, data=None, headers={'User-Agent': 'Python 3.6.1 (Windows NT 10.0):wallpaper-galpin:v1.0 (by /u/CodeOptimist)'})
+    return fetch_json()
+
+
+def fetch_json():
+    global json_data
+    json_url = "{}&after={}".format(url, after)
+    print(datetime.datetime.now(), "fetching", json_url)
+    req = urllib.request.Request(json_url, headers={'User-Agent': 'Python 3.6.1 (Windows NT 10.0):wallpaper-galpin:v2.0 (by /u/CodeOptimist)'})
     with urllib.request.urlopen(req, timeout=5) as response:
-        data_str = response.read().decode()
-        with open(path, 'w', encoding='utf-8') as f:
+        try:
+            data_str = response.read().decode()
+        except Exception:
+            time.sleep(60 * 3)
+            return False
+        with open(json_path, 'w', encoding='utf-8') as f:
             f.write(data_str)
-        data = json.loads(data_str)
+        json_data = json.loads(data_str)
     return True
 
 
-def load_wallpaper(directory, monitors, screen):
+def load_wallpaper(monitors, screen):
+    global after
     wallpaper = Image.new("RGB", (screen['w'], screen['h']))
     for idx, monitor in monitors.items():
-        img = get_img(directory, monitor)
-        print("found", idx)
-        img = PIL.ImageOps.fit(img, (monitor['w'], monitor['h']), PIL.Image.LANCZOS)
-        # img.save(os.path.join(directory, '{}.jpg'.format(idx)))
-        wallpaper.paste(img, (monitor['x'] - screen['x'], monitor['y'] - screen['y']))
-        # print(idx, monitor['x'], monitor['y'])
-    wallpaper_path = os.path.join(directory, 'wallpaper.jpg')
+        mon_rejects = 'rejects_{}_{}'.format(monitor['w'], monitor['h'])
+        if mon_rejects not in app_data:
+            app_data[mon_rejects] = set()
+
+        try:
+            for _json_attempt_idx in range(2):
+                for img_json in json_data['data']['children']:
+                    img_name = os.path.basename(img_json['data']['url'])
+                    if img_name in app_data['accepts'] | app_data['rejects'] | app_data[mon_rejects]:
+                        continue
+
+                    try:
+                        img, img_path = get_img(img_json, monitor)
+                        fit_img = PIL.ImageOps.fit(img, (monitor['w'], monitor['h']), PIL.Image.LANCZOS)
+                        name, ext = os.path.splitext(os.path.basename(img_path))
+                        fit_img.save(os.path.join(tmp_dir, name + '_fit' + ext))
+                        print(datetime.datetime.now(), "found", idx, os.path.basename(img_path), img_json['data']['title'])
+                        wallpaper.paste(fit_img, (monitor['x'] - screen['x'], monitor['y'] - screen['y']))
+                        # print(idx, monitor['x'], monitor['y'])
+                        app_data['accepts'].add(img_name)
+                        raise ImageSuccessError
+                    except MonitorImageRejectedError:
+                        app_data[mon_rejects].add(img_name)
+                    except ImageRejectedError:
+                        app_data['rejects'].add(img_name)
+                after = json_data['data']['after']
+                fetch_json()
+        except ImageSuccessError:
+            continue
+        return False
+
+    wallpaper_path = os.path.join(app_dir, 'wallpaper.jpg')
     wallpaper.save(wallpaper_path)
     ahk.execute(r'DllCall("SystemParametersInfo", UInt, 0x14, UInt, 0, Str, "{}", UInt, 2)'.format(wallpaper_path))
     # print(datetime.datetime.now(), "loaded")
+    return True
 
 
 def get_monitor_info():
@@ -96,70 +157,75 @@ def get_screen_info():
     return result
 
 
-def get_img(directory, monitor):
-    for img_json in data['data']['children']:
-        if 'is_checked' in img_json:
-            continue
-        img_json['is_checked'] = True
+class ImageSuccessError(Exception):
+    pass
 
-        title = img_json['data']['title']
-        link_flair_text = img_json['data']['link_flair_text'] or ''
-        dimension_re = r'[\[(]\s*(?P<w>\d+)\s*\D\s*(?P<h>\d+)[)\]]'
-        m = re.search(dimension_re, link_flair_text)
-        if m is None:
-            m = re.search(dimension_re, title)
-            if m is None:
-                continue
 
+class ImageRejectedError(Exception):
+    pass
+
+
+class MonitorImageRejectedError(Exception):
+    pass
+
+
+def get_img(img_json, monitor):
+    img_url = img_json['data']['url']
+    if '//imgur.com/' in img_url:
+        img_url = img_url.replace('//imgur.com/', '//i.imgur.com/') + '.jpg'
+    if not img_url.endswith(r'.jpg') and not img_url.endswith(r'.jpeg'):
+        raise ImageRejectedError
+
+    title = img_json['data']['title']
+    link_flair_text = img_json['data']['link_flair_text'] or ''
+    dimension_re = r'[\[(]\s*(?P<w>\d+)\s*\D\s*(?P<h>\d+)[)\]]'
+    m = re.search(dimension_re, link_flair_text)
+    if m is None:
+        m = re.search(dimension_re, title)
+
+    if m is not None:
         # can't tell for certain before downloading whether dimensions are given as WxH or HxW
         w = int(m.group('w'))
         h = int(m.group('h'))
         if min(w, h) < min(monitor['w'], monitor['h']):
-            continue
+            raise MonitorImageRejectedError
         if max(w, h) < max(monitor['w'], monitor['h']):
-            continue
+            raise MonitorImageRejectedError
 
-        img_url = img_json['data']['url']
-        if '//imgur.com/' in img_url:
-            img_url = img_url.replace('//imgur.com/', '//i.imgur.com/') + '.jpg'
-        if not img_url.endswith(r'.jpg') and not img_url.endswith(r'.jpeg'):
-            continue
+    img_path = fetch_img(img_url)
+    img = Image.open(img_path)
+    img.load()  # immediately closes file
 
-        try:
-            img_path = fetch_img(directory, img_url, title)
-        except Exception:
-            continue
+    w, h = img.size
+    if w < monitor['w'] or h < monitor['h']:
+        raise MonitorImageRejectedError
 
-        img = Image.open(img_path)
-        w, h = img.size
-        if w < monitor['w'] or h < monitor['h']:
-            continue
+    is_oriented_same = (monitor['w'] / monitor['h'] >= 1) == (w / h >= 1)
 
-        is_oriented_same = (monitor['w'] / monitor['h'] >= 1) == (w / h >= 1)
-        if not is_oriented_same:
-            # tends to look bad
-            continue
+    img_ar = w / h
+    mon_ar = monitor['w'] / monitor['h']
+    fit_percent = (mon_ar * h) / w if img_ar >= mon_ar else (w / mon_ar) / h
+    is_fit = fit_percent >= required_fit_percent
 
-        return img
-    return None
+    if not is_oriented_same or not is_fit:
+        raise MonitorImageRejectedError
+    return img, img_path
 
 
-def fetch_img(directory, img_url, title):
-    img_filename = os.path.basename(img_url)
-    img_path = os.path.join(directory, img_filename)
-
+def fetch_img(img_url):
+    img_name = os.path.basename(img_url)
+    img_path = os.path.join(tmp_dir, img_name)
     if os.path.isfile(img_path):
         age = time.time() - os.path.getmtime(img_path)
         is_old_file = age > minute_interval * 60
         if is_old_file:
             raise FileExistsError
-        print(datetime.datetime.now(), "       ", img_path, title)
     else:
         with urllib.request.urlopen(img_url) as response:
             img_data = response.read()
         with open(img_path, "wb") as f:
             f.write(img_data)
-        print(datetime.datetime.now(), "fetched", img_path, title)
+        time.sleep(5)
     return img_path
 
 
